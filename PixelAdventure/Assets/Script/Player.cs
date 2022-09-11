93418f5ff6958f6279950668fec230b764cf3eb4
Gabriel Martinho <gabrielmartinho02@gmail.com>
2022-09-11 18:26:11 -0300

    pulo dublo e Queda

--- PixelAdventure/Assets/Script/Player.cs
+++ PixelAdventure/Assets/Script/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     Animator anim;
     SpriteRenderer rend;
     public float speed, jumpForce = 0f;
+    private bool isJumping, doubleJump;
     private float inputHorizontal;
 
     // Start is called before the first frame update
@@ -22,19 +23,42 @@ public class Player : MonoBehaviour
     void Update()
     {
         Inputs();
-        VerificationAnimator();
     }
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
-        
+
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            isJumping = false;
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isDoubleJump", false);
+            anim.SetBool("isFall", false);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            isJumping = true;
+            anim.SetBool("isJumping", true);
+        }
+    }
+
     private void Inputs()
     {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
 
         if (inputHorizontal != 0)
         {
+            anim.SetBool("isRun", true);
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                anim.SetBool("isJumping", true);
+            }
             if (inputHorizontal > 0)
             {
                 rend.flipX = false;
@@ -45,22 +69,30 @@ public class Player : MonoBehaviour
                 rend.flipX = true;
             }
         }
+        else { anim.SetBool("isRun", false); }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            if (!isJumping)
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                    anim.SetBool("isDoubleJump", true);
+                    rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);;
+                    doubleJump = false;
+                }
+                else
+                {
+                    anim.SetBool("isFall", true);
+                }
+            }
         }
 
-    }
-
-    private void VerificationAnimator()
-    {
-        if(inputHorizontal != 0)
-        {
-            anim.SetBool("isRun", true);
-        }
-        else
-        {
-            anim.SetBool("isRun", false);
-        }
     }
 }
+
